6da9c99214e82cad99e3c235d6dad055f32fa574
AlanCCBP <36832249+AlanCCBP@users.noreply.github.com>
2019-06-16 15:54:29 -0300

    Add files via upload

---
+++ What's the weather/What's the weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What's the weather
+//
+//  Created by Alan Nevot on 22/7/17.
+//  Copyright © 2017 Alan Nevot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
